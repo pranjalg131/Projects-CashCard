e08a4e7535875b67793fa9109e4e7f47d6190a1d
Pranjal Gupta <pranjalg131@gmail.com>
2024-06-29 01:45:33 +0530

    feat: add database and data repository

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -6,15 +6,26 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/cashcards")
 class CashCardController {
+    private final CashCardRepository cashCardRepository;
+
+    private CashCardController(CashCardRepository cashCardRepository) {
+        this.cashCardRepository = cashCardRepository;
+    }
+
     @GetMapping("/{requestedId}")
     private ResponseEntity<CashCard> findById( @PathVariable Long requestedId ) {
         if(requestedId != 99L)
             return ResponseEntity.notFound().build();
 
-        CashCard cashCard = new CashCard(99L, 123.45);
-        return ResponseEntity.ok(cashCard);
+        Optional<CashCard> cashCardOptional = cashCardRepository.findById(requestedId);
+        if(cashCardOptional.isEmpty())
+            return ResponseEntity.notFound().build();
+
+        return ResponseEntity.ok(cashCardOptional.get());
     }
 }
